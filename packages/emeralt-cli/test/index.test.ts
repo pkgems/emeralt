6ada748e665cebf8c092280fe89b2bc96c793180
stackdumper <stackdumper@gmail.com>
2018-12-26 13:11:36 +0200

    test(cli): add test

--- packages/emeralt-cli/test/index.test.ts
+++ packages/emeralt-cli/test/index.test.ts
@@ -1,5 +1,12 @@
 import test from 'ava'
+import supertest from 'supertest'
 
-test('ok', (t) => {
+test('ok', async (t) => {
+  require('../src/index')
+
+  await supertest('http://localhost:8080')
+    .get('/-/ping')
+    .expect(200)
+
   t.pass()
 })
