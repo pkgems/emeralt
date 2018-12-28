1990bbc8a728c99cb5a4ee72a90a4824a0ef2b2d
stackdumper <stackdumper@gmail.com>
2018-12-28 12:40:35 +0200

    test(server): extend storage mock

--- packages/emeralt-server/test/fixtures/mock-storage.ts
+++ packages/emeralt-server/test/fixtures/mock-storage.ts
@@ -1,3 +1,16 @@
 import { IEmeraltStorage } from '@emeralt/types'
+import sinon from 'sinon'
 
-export class MockStorage implements IEmeraltStorage {}
+export class MockStorage implements IEmeraltStorage {
+  getTarball = sinon.fake(
+    (name: string): void | string => {
+      throw new Error('Method not implemented.')
+    },
+  )
+
+  putTarball = sinon.fake(
+    (name: string, tarball: string): boolean => {
+      throw new Error('Method not implemented.')
+    },
+  )
+}
