a85894219acb5354f7c034285d912f94ed8ed5f8
stackdumper <stackdumper@gmail.com>
2018-12-28 12:05:10 +0200

    build: use child process for watch

--- cli/actions/watch.ts
+++ cli/actions/watch.ts
@@ -1,10 +1,61 @@
 import chokidar from 'chokidar'
 import { readFile } from 'fs-extra'
-import { build } from './build'
+import { spawn } from 'child_process'
+import { join } from 'path'
 import { getRootDir } from '../utils'
 
+const spawnBuild = () =>
+  new Promise((resolve, reject) => {
+    const proc = spawn(
+      'node',
+      [
+        '--max_old_space_size=4096',
+        join(__dirname, '../', 'index.js'),
+        'build',
+      ],
+      {
+        stdio: 'inherit',
+      },
+    )
+
+    proc.on('close', (code) => {
+      if (code === 0) resolve()
+      else reject()
+    })
+  })
+
+const createBuilder = () => {
+  let building = false
+  let buildQueued = false
+
+  const rebuild = async (queued = false) => {
+    console.log('\nRebuilding', queued ? 'queued' : '')
+
+    if (!building) {
+      building = true
+      try {
+        await spawnBuild()
+      } catch (error) {
+        console.error(error)
+      }
+      building = false
+
+      if (buildQueued && !queued) {
+        await rebuild(true)
+      }
+
+      building = false
+    } else {
+      console.log('Queuing build')
+      buildQueued = true
+    }
+  }
+
+  return rebuild
+}
+
 export const watch = async (pkg: string | void, options) => {
-  await build(pkg, options)
+  await spawnBuild()
 
   const watcher = chokidar.watch('**/*', {
     cwd: getRootDir(),
@@ -19,36 +70,6 @@ export const watch = async (pkg: string | void, options) => {
     ],
     interval: 100,
   })
-
-  const createBuilder = () => {
-    let building = false
-    let buildQueued = false
-
-    const rebuild = async (queued = false) => {
-      console.log('\nRebuilding', queued ? 'queued' : '')
-
-      if (!building) {
-        building = true
-        try {
-          await build(pkg, options)
-        } catch (error) {
-          console.error(error)
-        }
-        building = false
-
-        if (buildQueued && !queued) {
-          await rebuild(true)
-        }
-
-        building = false
-      } else {
-        console.log('Queuing build')
-        buildQueued = true
-      }
-    }
-
-    return rebuild
-  }
 
   const builder = createBuilder()
 
