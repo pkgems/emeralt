9c69ceea1b7b4bc5cdeacafa936354664842c99c
stackdumper <stackdumper@gmail.com>
2019-01-08 21:15:58 +0200

    feat(server): add integrity verification on publish

--- packages/core/emeralt-server/src/handlers/packages/publish-package.ts
+++ packages/core/emeralt-server/src/handlers/packages/publish-package.ts
@@ -1,9 +1,10 @@
 import { TEmeraltHandlerParams, TPackage, TVersion } from '@emeralt/types'
 import { endpoints } from '@/constants'
 import { Router } from 'express'
-import { pipe, dissoc, path, values, head, assocPath } from 'ramda'
-import crypto from 'crypto'
+import { pipe, dissoc } from 'ramda'
+import ssri from 'ssri'
 
+// filter keys with pure js in a few lines?
 const getMetadata = pipe(
   dissoc('versions'),
   dissoc('_attachments'),
@@ -14,29 +15,16 @@ const getTarballUrl = (v: TVersion) =>
     v.name,
   )}/${encodeURIComponent(v.version)}`
 
-const getVersion = pipe(
-  path(['versions']),
-  values,
-  head,
-  (v: any) => assocPath(['dist', 'tarball'], getTarballUrl(v), v),
-)
+const getVersion = (pkg: TPackage) => Object.values(pkg.versions).shift()
 
-const getTarball = pipe(
-  path(['_attachments']),
-  values,
-  head,
-)
+const getTarball = (pkg: TPackage) => {
+  const tb = Object.values(pkg._attachments).shift()
 
-// const verifyShasum = (integrity, data) => {
-//   const [algo, sum] = integrity.split('-')
-
-//   const orig = crypto
-//     .createHash(algo)
-//     .update(data, 'utf8')
-//     .digest('base64')
-
-//   return orig === sum
-// }
+  return {
+    ...tb,
+    data: Buffer.from(tb.data, 'base64'),
+  }
+}
 
 export const publishpackage = ({
   middlewares,
@@ -50,18 +38,22 @@ export const publishpackage = ({
 
       const pkg = req.body as TPackage
 
-      const metadata = getMetadata(pkg)
-      const version = getVersion(pkg)
-      const tarball = getTarball(pkg) as any
+      const metadata = getMetadata(pkg) as TPackage
+      const version = getVersion(pkg) as TVersion
+      const tarball = getTarball(pkg)
+
+      if (!(await ssri.checkData(tarball.data, version.dist.integrity))) {
+        return res.status(400).json({
+          ok: false,
+          message: 'Integrity verification failed',
+        })
+      }
+
+      version.dist.tarball = getTarballUrl(version)
 
       await database.putMetadata(pkg.name, metadata)
       await database.putVersion(pkg.name, version.version, version)
-      await storage.putTarball(
-        pkg.name,
-        version.version,
-        // @ts-ignore
-        Buffer.from(tarball.data, 'base64'),
-      )
+      await storage.putTarball(pkg.name, version.version, tarball.data)
 
       res.status(200).json({})
     })
