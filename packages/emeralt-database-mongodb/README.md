85df6d207414b2fd8b2d54ff0dbfa0f1cb748cbf
Viacheslav Yefremov <45338622+viacheslavyefremov@users.noreply.github.com>
2019-03-05 19:28:58 +0200

    feat: add database-mongodb (#71)

---
+++ packages/emeralt-database-mongodb/README.md
@@ -0,0 +1,22 @@
+# @emeralt/database-mongodb
+Emeralt database plugin to store packages in MongoDB
+
+## Install
+
+Using npm:
+
+```sh
+npm install --save-dev @emeralt/database-mongodb
+```
+
+or using yarn:
+
+```sh
+yarn add --dev @emeralt/database-mognodb
+```
+
+## Usage
+
+```ts
+new EmeraltDatabaseMongoDB()
+```
---
+++ packages/emeralt-database-mongodb/src/index.ts
@@ -0,0 +1,120 @@
+import {
+  CEmeraltDatabase,
+  IEmeraltDatabase,
+  TVersion,
+  TMetadata,
+} from '@emeralt/types'
+import { MongoClient, Db, Collection } from 'mongodb'
+
+type Options = {
+  url: string
+  dbName: string
+}
+
+class CEmeraltDatabaseMongoDB implements CEmeraltDatabase {
+  storage: {
+    metadatas: Collection
+    versions: Collection
+  }
+
+  constructor(private db: Db) {
+    this.storage = {
+      metadatas: this.db.collection('metadatas'),
+      versions: this.db.collection('versions'),
+    }
+  }
+
+  public getMetadatas() {
+    return this.storage.metadatas
+      .find({}, { projection: { _id: false } })
+      .toArray()
+      .then((metadatas) =>
+        metadatas.reduce(
+          (acc, curr) => ({
+            ...acc,
+            [curr.name]: curr,
+          }),
+          {},
+        ),
+      )
+  }
+
+  public hasMetadata(name: string) {
+    return this.storage.metadatas
+      .find({ name })
+      .limit(1)
+      .count()
+      .then((c) => c === 1)
+  }
+
+  public getMetadata(name: string) {
+    return this.storage.metadatas.findOne(
+      { name },
+      { projection: { _id: false } },
+    )
+  }
+
+  public putMetadata(name: string, data: TMetadata) {
+    return this.storage.metadatas.findOneAndUpdate(
+      { name },
+      { $set: { ...data } },
+      { upsert: true },
+    )
+  }
+
+  public getVersions(name: string) {
+    return this.storage.versions.findOne({ name }).then((v) =>
+      Object.keys(v ? v.versions : {}).reduce(
+        (acc, key) => ({
+          ...acc,
+          ...{ [key.replace(/-/g, '.')]: v.versions[key] },
+        }),
+        {},
+      ),
+    )
+  }
+
+  public hasVersion(name: string, version: string) {
+    return this.getVersion(name, version).then((v) => Boolean(v))
+  }
+
+  public getVersion(name: string, version: string) {
+    return this.getVersions(name).then((versions) => versions[version])
+  }
+
+  public putVersion(name: string, version: string, data: TVersion) {
+    return this.storage.versions.findOneAndUpdate(
+      { name },
+      { $set: { versions: { [version.replace(/\./g, '-')]: { ...data } } } },
+      { upsert: true },
+    )
+  }
+
+  public dropData() {
+    return this.db.dropDatabase()
+  }
+
+  public async healthz() {
+    try {
+      await this.db.command({ ping: 1 })
+
+      return { ok: true }
+    } catch (error) {
+      return {
+        ok: false,
+        message: error.message,
+        error: error,
+      }
+    }
+  }
+}
+
+export const EmeraltDatabaseMongoDB: IEmeraltDatabase<Options> = ({
+  url = 'mongodb://localhost:27017',
+  dbName = 'emeralt-test',
+}) => async () => {
+  const client = await new MongoClient(url, { useNewUrlParser: true }).connect()
+  const db = client.db(dbName)
+
+  return new CEmeraltDatabaseMongoDB(db)
+}
