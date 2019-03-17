19bb10de2b67cddf8d38f729d118fc82398bb43a
 stackdumper <stackdumper@gmail.com>
2019-03-17 12:49:52 +0200

    test(cli): remove source test

--- packages/emeralt-cli/test/index.test.ts
+++ packages/emeralt-cli/test/index.test.ts
@@ -1,31 +1,19 @@
 import test from 'ava'
 import supertest from 'supertest'
-import { spawn } from 'child_process'
+import { fork } from 'child_process'
 
-test('start server', async (t) => {
-  require('../src/index')
+const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t))
+
+test.serial('start server', async (t) => {
+  const server = fork('build/index.js')
+
+  await sleep(1000)
 
   await supertest('http://localhost:8080')
     .get('/-/ping')
     .expect(200)
 
+  server.kill('SIGTERM')
+
   t.pass()
-})
-
-test('start built server', async (t) => {
-  const { status, body } = await new Promise(async (resolve) => {
-    const server = spawn('node', ['build/index.js'])
-
-    const { status, body } = await supertest('http://localhost:8080').get(
-      '/-/ping',
-    )
-
-    server.kill('SIGTERM')
-    server.on('close', () => {
-      resolve({ status, body })
-    })
-  })
-
-  t.is(status, 200)
-  t.deepEqual(body, {})
 })
